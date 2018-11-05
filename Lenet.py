7f953f0c76a61db74c1d71834ffcf3677428fc02
quartz0714 <573109868@qq.com>
2018-11-05 15:12:24 +0800

    update Lenet.py

--- Lenet.py
+++ Lenet.py
@@ -12,27 +12,27 @@ x_image = tf.reshape(x,[-1,28,28,1])
 filter1 = tf.Variable(tf.truncated_normal([5,5,1,6]))
 bias1 = tf.Variable(tf.truncated_normal([6]))
 conv1 = tf.nn.conv2d(x_image,filter1,strides=[1,1,1,1],padding='SAME')
-h_conv1 = tf.nn.relu(conv1+bias1)
+h_conv1 = tf.nn.sigmoid(conv1+bias1)
 
 maxPool2 = tf.nn.max_pool(h_conv1,ksize=[1,2,2,1],strides=[1,2,2,1],padding='SAME')
 
 filter2 = tf.Variable(tf.truncated_normal([5,5,6,16]))
 bias2 = tf.Variable(tf.truncated_normal([16]))
 conv2 = tf.nn.conv2d(maxPool2,filter2,strides=[1,1,1,1],padding='SAME')
-h_conv2 = tf.nn.relu(conv2+bias2)
+h_conv2 = tf.nn.sigmoid(conv2+bias2)
 
 maxPool3 = tf.nn.max_pool(h_conv2,ksize=[1,2,2,1],strides=[1,2,2,1],padding='SAME')
 
 filter3 = tf.Variable(tf.truncated_normal([5,5,16,120]))
 bias3 = tf.Variable(tf.truncated_normal([120]))
 conv3 = tf.nn.conv2d(maxPool3,filter3,strides=[1,1,1,1],padding='SAME')
-h_conv3 = tf.nn.relu(conv3+bias3)
+h_conv3 = tf.nn.sigmoid(conv3+bias3)
 
 W_fc1 = tf.Variable(tf.truncated_normal([7*7*120,80]))
 b_fc1 = tf.Variable(tf.truncated_normal([80]))
 
 h_pool2_flat = tf.reshape(h_conv3,[-1,7*7*120])
-h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat,W_fc1)+b_fc1)
+h_fc1 = tf.nn.sigmoid(tf.matmul(h_pool2_flat,W_fc1)+b_fc1)
 
 W_fc2 = tf.Variable(tf.truncated_normal([80,10]))
 b_fc2 = tf.Variable(tf.truncated_normal([10]))
